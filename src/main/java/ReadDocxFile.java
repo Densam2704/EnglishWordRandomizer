2e3d5810d487f5c4af4b4235fb6d99cdc50b4de6
Densam2704 <den.samoshckin@yandex.ru>
2021-04-30 16:13:00 +0300

    Added methods for generating random words list

--- src/main/java/ReadDocxFile.java
+++ src/main/java/ReadDocxFile.java
@@ -1,7 +1,8 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
-import org.apache.poi.hwpf.usermodel.TableRow;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFTable;
@@ -10,13 +11,21 @@ import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 public class ReadDocxFile
 {
   public static final String WORD_FILE = "C:\\Study\\English\\Vocabulary_2.docx";
+  public static final int NUMBER_OF_WORDS = 10;
+  
 //public static final String WORD_FILE =
 //		"C:\\Study\\Programming\\PC projects\\Everyday learning\\EnglishWordRandomizer\\data\\docx files\\Test file.docx";
 //
+  public static  ArrayList<String> wordsAndTranslations = new ArrayList<String>();
+  public static  ArrayList<String>randomWordsList = new ArrayList<String>();
+  
   public static void main(String[] args)
   {
 	readDocxFile(WORD_FILE);
+	makeRandomWordsList(NUMBER_OF_WORDS);
+	printRandomWordsList();
   }
+  
   
   private static void readDocxFile(String docxFileName) {
 	try {
@@ -24,9 +33,7 @@ public class ReadDocxFile
 	  FileInputStream fis = new FileInputStream(file.getAbsolutePath());
 	  
 	  XWPFDocument document = new XWPFDocument(fis);
-	  readParagraphsInXWPFDocument(document);
-	  readTableInXWPFDocument(document);
-	  
+	  readTableFromXWPFDocument(document);
 	 
 	  fis.close();
 	} catch (Exception e) {
@@ -34,7 +41,7 @@ public class ReadDocxFile
 	}
   }
   
-  private static void readParagraphsInXWPFDocument(XWPFDocument document){
+  private static void readParagraphsFromXWPFDocument(XWPFDocument document){
 	List<XWPFParagraph> paragraphs = document.getParagraphs();
  
 	for (XWPFParagraph paragraph : paragraphs) {
@@ -42,14 +49,59 @@ public class ReadDocxFile
 	}
   }
   
-  private static void readTableInXWPFDocument(XWPFDocument document){
+  private static void readTableFromXWPFDocument(XWPFDocument document){
 	List<XWPFTable> tables = document.getTables();
 	
 	for (XWPFTable table : tables) {
-	  System.out.println(table.getText());
-//	  for (XWPFTableRow row:rows){
-//		System.out.println(row.);
+	  
+	  List<XWPFTableRow> rows = table.getRows();
+	  //We don't need first row, we start from the second.
+	  for (int i = 1; i< rows.size();i++) {
+		try {
+		  XWPFTableRow row = rows.get(i);
+		  readWordsAndTranslationsFromRow(row);
+		}
+		catch (Exception e){
+		}
+	  }
 	}
-	
+		
   }
+  
+  private static void readWordsAndTranslationsFromRow(XWPFTableRow row){
+	String result="";
+	if (row.getCell(0).getText().equals("")||row.getCell(2).getText().equals("")){
+	  return;
+	}
+	result+=row.getCell(0).getText();
+	result+="\t\t\t";
+	result+=row.getCell(2).getText();
+//	System.out.println(result);
+	
+	wordsAndTranslations.add(result);
+	}
+  
+  
+  private static void makeRandomWordsList(int listSize) {
+    ArrayList<String>copyList = wordsAndTranslations;
+	for (int i = 0; i < listSize; i++) {
+	  //random int from 0 to copyList.size()
+	  int rNum = getRandomInt(0,copyList.size());
+	  randomWordsList.add(copyList.get(rNum));
+	  copyList.remove(rNum);
+	}
+  }
+  
+  private static int getRandomInt(int min, int max){
+	Random r = new Random();
+	return r.nextInt((max - min) + 1) + min;
+  }
+  
+  
+  private static void printRandomWordsList() {
+	for(String s:randomWordsList){
+	  System.out.println(s);
+	}
+  }
+  
 }
